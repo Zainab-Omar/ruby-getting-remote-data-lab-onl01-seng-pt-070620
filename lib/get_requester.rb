1826d4de0f5895fa3fb6d6fdb8775f3a3e75b014
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-08-12 20:33:43 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,28 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester 
+  attr_accessor :url
+  def initialize (url)
+    @url=url
+  end
+  
+  def get_response_body 
+    data_request = GetRequester.new (url)
+    
+    
+  end
+  
+  def parse_json 
+    
+  end
+  
+end
+
+
+# def get_programs
+#     uri = URI.parse(URL)
+#     response = Net::HTTP.get_response(uri)
+#     response.body
+#   end
